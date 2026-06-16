f30a7942e84b12d6d58c071b357d490ea03814cb
Raheel Ahmed <raheel25031@gmail.com>
2026-06-16 23:08:27 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import createServer from 'node:http'
+
+
+const app = express()
+const server = createServer(app)
+ 
+server.listen(3000, () => console.log("server running"));
