b0937e963ae0fbd949df587c7952a1ee2b2c4ba1
Raheel Ahmed <raheel25031@gmail.com>
2026-06-18 12:42:13 +0500

    message send message recive socket connection connect and disconnect

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import express from 'express'
-import createServer from 'node:http'
-
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 const app = express()
 const server = createServer(app)
+const io = new Server(server)
+
+app.use(express.static('public'))
+
+io.on('connection', (socket) => {
+    console.log("message pass", socket.id)
+
+    socket.on('message', (msg) => {
+        console.log("message", msg);
+        io.emit('message',msg)
+    })
+})
+
+
+app.get('/', (req, res) => {
+    return res.sendFile('index.html')
+})
  
 server.listen(3000, () => console.log("server running"));
